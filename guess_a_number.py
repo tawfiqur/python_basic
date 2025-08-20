fdfba51e9d733e45e2a21c8b804351d202d2036f
tawfiqur <tawfiq.rm@gmail.com>
2025-08-20 22:24:20 +0600

    logic updated

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,15 +1,23 @@
-guess_number = 23
-guess_count = 0
-guess_limit = 3
+import random
 
-while guess_count < guess_limit:
-    guess = int(input("Guess a two digit number: "))
-    guess_count += 1
-    if guess == guess_number:
-        print('You guessed right!')
-        break
-else:
-    print('Sorry, you didn\'t guess right!')
-    print('Try again!')
-    guess_limit = guess_count - 1
+secret_number = random.randint(1, 100)
+guess = 0
 
+attempts = 0
+max_attempts = 7
+
+while attempts < max_attempts:
+    print(f"Attempt - {attempts}")
+    guess = int(input())
+    if guess < 1 or guess > 100:
+        print("Please enter a number between 1 and 100")
+        continue
+    attempts += 1
+    if guess < secret_number:
+        print("Your guess is too low")
+    elif guess > secret_number:
+        print("Your guess is too high")
+    else:
+        print(f"Congrats!!! You win. You guessed {guess}")
+if guess != secret_number:
+    print(f"Sorry, you lose!!! Correct number was {secret_number}")
